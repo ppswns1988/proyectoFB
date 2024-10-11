6913c9de7282fef636bbcdb7882e35dd08404dd5
PabloPetidier <petidiersmit@gmail.com>
2024-10-11 18:16:52 +0200

    Prettier

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-"use strict";
-import dotenv from "dotenv";
+'use strict';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 const { PORT } = process.env;
 
-// login
-app.get("/login", function (req, res) {
-    res.send("Login");
+//login
+app.get('/login', function (req, res) {
+    res.send('Login');
 });
 
 app.listen(PORT, () => {
     console.log(`http://127.0.0.1:${PORT}`);
 });
-
